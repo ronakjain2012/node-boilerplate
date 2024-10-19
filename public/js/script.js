21d8dec736d7a97452cc982030e78ddd3e7f4a22
Ronak Bokaria <Ronakjain2012@gmail.com>
2024-10-19 15:29:47 +0530

    � Clean

--- public/js/script.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable no-undef */
-// let root = document.documentElement;
-//     root.style.setProperty('--primary', '#da12af');
--- utils/globalHelpers.js
+++
@@ -1,5 +0,0 @@
-const logger = require('./../config/others/winston.js');
-const apiResponse = require('./apiResponse.js');
-global.logger = logger;
-global.apiResponse = apiResponse;
-module.exports = apiResponse;
